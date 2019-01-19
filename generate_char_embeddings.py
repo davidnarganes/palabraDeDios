1834e768c9a604c70b023824108da7a9b29b4bf4
davidnarganes <david.narganes95@gmail.com>
2019-01-19 22:47:55 +0000

    embeddings done

--- generate_char_embeddings.py
+++ generate_char_embeddings.py
@@ -1,13 +1,90 @@
+import string
 from gensim.models import Word2Vec
 
-with open('data/Biblia/procesado_1/biblia_procesada_1.txt','r', encoding='utf-8') as handle:
+def tokenise_char(sentences):
+    return_ = list()
+    for s in sentences:
+        for tup in remplace_list:
+            s = s.replace(tup[0],tup[1])
+        return_.append(s.split())
+    return return_
+
+def flat_list(nested_list):
+    while any([isinstance(x,list) for x in nested_list]):
+        nested_list = [item for sublist in nested_list for item in sublist]
+    return nested_list
+
+with open('data/Biblia/procesado_1/biblia_procesada_1.txt','r', encoding='latin1') as handle:
     biblia_raw = handle.readlines()
 
 # Define some vars to replace
-white_space = '<ws>'
+biblia_remplaced = [s.split() for s in biblia_raw]
+remplace_list = [
+('á',' a <tilde> '),
+('é',' e <tilde> '),
+('í',' i <tilde> '),
+('ó',' o <tilde> '),
+('ú',' u <tilde> '),
+('\n',' <end_line> ')
+]
+
+list_chars = [
+    'a',
+ 'b',
+ 'c',
+ 'd',
+ 'e',
+ 'f',
+ 'g',
+ 'h',
+ 'i',
+ 'j',
+ 'k',
+ 'l',
+ 'm',
+ 'n',
+ 'o',
+ 'p',
+ 'q',
+ 'r',
+ 's',
+ 't',
+ 'u',
+ 'v',
+ 'w',
+ 'x',
+ 'y',
+ 'z',
+ 'ñ',
+ 'ü',
+]
+
+remplace_list = [(char,' {} '.format(char)) for char in list_chars] + remplace_list
 unknown = '<unk>'
 pad = '<pad>'
 
+# Replace
+biblia_rep = tokenise_char(biblia_raw)
+
+# Verify chars
+chars = set(flat_list(biblia_rep))
+
+# Run w2v
+epochs = int(1e6)
 model = Word2Vec(size=300, window=10, min_count=1, workers=-1)
-model.build_vocab(char_sent_biblia)
-model.train(char_sent_biblia, total_examples=model.corpus_count, epochs=10)
+model.build_vocab(biblia_rep)
+%timeit model.train(biblia_rep, total_examples=model.corpus_count, epochs=epochs)
+
+char_embedding = model.wv.vectors
+chars = list(model.wv.vocab.keys())
+
+# Visualise
+from sklearn.manifold import TSNE
+tsne = TSNE(n_iter=10000)
+embedding_2d = tsne.fit_transform(char_embedding)
+
+from matplotlib import pyplot as plt
+plt.scatter(embedding_2d[:,0], embedding_2d[:,1])
+for char,loc in zip(chars, embedding_2d):
+    plt.text(loc[0], loc[1], char)
+plt.show()
