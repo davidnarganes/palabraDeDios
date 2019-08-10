25fe7ffb7a79db3b40d18de1b862feb8bf73042f
davidnarganes <david.narganes95@gmail.com>
2019-08-10 18:58:26 +0100

    hey

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,7 +2,7 @@ import os
 
 def mknewdir(newdir):
     if os.path.exists(newdir):
-        print("'%s' already exists" % newdir)
+        print("`%s` already exists" % newdir)
     else:
         os.mkdir(newdir)
-        print("'%s' created" % newdir)
+        print("`%s` created" % newdir)
